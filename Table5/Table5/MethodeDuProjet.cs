e4c10f0074c03affef764068c756bc888803854a
NathLeem <91312993+NathLeem@users.noreply.github.com>
2021-10-07 16:13:15 +0200

    upload du programme sur les table de 5

---
+++ Table5/Table5/MethodeDuProjet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Table5
+{
+    public struct MethodeDuProjet
+    {
+        public void Table5(int N,int valeur,out int Result)
+        {
+            while (N = 20)
+            {
+                valeur = N * 5;
+                Result = Result + "," + valeur;
+            }
+        }
+    }
+}
---
+++ Table5/Table5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Table5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int Result;
+            int valeur;
+
+            Console.WriteLine("entrez une valeur:");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+            MesOutils.Table5(valeur,N,out Result);
+
+            Console.WriteLine($"Voici tout les multiples de 5 jusque 500: {Result}");
+        }
+    }
+}
